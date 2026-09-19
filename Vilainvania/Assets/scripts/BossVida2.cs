78f29ac9cb83b584f7e92174924e5abc2479d6b3
bernardols2023-Lopes <bernardols2023@gmail.com>
2026-09-19 08:32:28 -0300

    .

--- Vilainvania/Assets/scripts/BossVida2.cs
+++ Vilainvania/Assets/scripts/BossVida2.cs
@@ -9,19 +9,25 @@ public class BossVida2 : MonoBehaviour
     private int vidaAtuais;
     [SerializeField] TMP_Text vidaBoss;
 
+    // Apenas criamos a referência para o Animator aqui
+    private Animator anim;
+
     void Start()
     {
-        
         vidaAtuais = vidaMaxima;
+
+        // Pegamos o componente Animator do Boss ao iniciar
+        anim = GetComponent<Animator>();
     }
 
-    
     public void TonarDano(int quantidadeDano)
     {
         vidaAtuais -= quantidadeDano;
         Debug.Log("Boss tomou dano! Vida atual: " + vidaAtuais);
 
-        
+        // ESSA É A ÚNICA LINHA NOVA: Ela ativa a animação Hurt quando ele toma dano!
+        if (anim != null) anim.SetTrigger("hurt");
+
         if (vidaAtuais <= 0)
         {
             Vitoria1();
@@ -32,7 +38,6 @@ public class BossVida2 : MonoBehaviour
     {
         Debug.Log("Você derrotou o boss");
 
-       
         SceneManager.LoadScene("Vitoria 1");
     }
 
